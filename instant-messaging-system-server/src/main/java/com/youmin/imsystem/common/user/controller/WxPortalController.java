8c8e4b772dd12e326aedceeced44d28ebe4317b7
PUIYOUMIN <youminpui@gmail.com>
2024-07-13 00:58:51 +0800

    Generate wechat user with sceneid; Send authentication url to user; get user info from wechat by access token after user has clicked the authentication url in wechat official account

--- instant-messaging-system-server/src/main/java/com/youmin/imsystem/common/user/controller/WxPortalController.java
+++ instant-messaging-system-server/src/main/java/com/youmin/imsystem/common/user/controller/WxPortalController.java
@@ -61,17 +61,11 @@ public class WxPortalController {
     }
 
     @GetMapping("/callBack")
-    public RedirectView callBack(@RequestParam String code) {
-        try {
-            WxOAuth2AccessToken accessToken = wxService.getOAuth2Service().getAccessToken(code);
-            WxOAuth2UserInfo userInfo = wxService.getOAuth2Service().getUserInfo(accessToken, "zh_CN");
-//            wxMsgService.authorize(userInfo);
-        } catch (Exception e) {
-            log.error("callBack error", e);
-        }
-        RedirectView redirectView = new RedirectView();
-        redirectView.setUrl("https://mp.weixin.qq.com/s/m1SRsBG96kLJW5mPe4AVGA");
-        return redirectView;
+    public RedirectView callBack(@RequestParam String code) throws WxErrorException {
+        WxOAuth2AccessToken accessToken = wxService.getOAuth2Service().getAccessToken(code);
+        WxOAuth2UserInfo userInfo = wxService.getOAuth2Service().getUserInfo(accessToken, "zh_CN");
+        System.out.println(userInfo);
+        return null;
     }
 
     @PostMapping(produces = "application/xml; charset=UTF-8")
--- instant-messaging-system-server/src/main/java/com/youmin/imsystem/common/user/service/handler/ScanHandler.java
+++ instant-messaging-system-server/src/main/java/com/youmin/imsystem/common/user/service/handler/ScanHandler.java
@@ -1,5 +1,6 @@
 package com.youmin.imsystem.common.user.service.handler;
 
+import com.youmin.imsystem.common.user.service.adapter.TextBuilder;
 import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.error.WxErrorException;
@@ -7,17 +8,27 @@ import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.net.URLEncoder;
 import java.util.Map;
 
 @Component
 public class ScanHandler extends AbstractHandler{
+
+    @Value("${imsystem.wx.callback}")
+    private String callback;
+
+    private static final String URL = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+
     @Override
-    public WxMpXmlOutMessage handle(WxMpXmlMessage wxMpXmlMessage, Map<String, Object> map, WxMpService wxMpService, WxSessionManager wxSessionManager) throws WxErrorException {
-        String fromUser = wxMpXmlMessage.getFromUser();
-        String eventKey = wxMpXmlMessage.getEventKey();
-        this.logger.info("User Scan Event From Wechat, Scene ID:{}, User's Openid",eventKey,fromUser);
-        return null;
+    public WxMpXmlOutMessage handle(WxMpXmlMessage wxMpXmlMessage, Map<String, Object> map,
+                                    WxMpService wxMpService, WxSessionManager wxSessionManager) throws WxErrorException {
+        String authenticateUrl = String.format(URL, wxMpService.getWxMpConfigStorage().getAppId(), URLEncoder.encode(callback+"/wx/portal/public/callBack"));
+        System.out.println(authenticateUrl);
+        return TextBuilder.build("Please click the link to authenticate: " + "<a href=\"" + authenticateUrl +
+                        "\">Register Imsystem Account</a>", wxMpXmlMessage);
+
     }
 }
